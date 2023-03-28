1385076a474791535eb8dd55a4bf21d9b0907ee5
IsraelEidan <israel.ghsti@gmail.com>
2023-03-28 21:05:20 +0300

    israel eidan 28.03

--- client/src/components/LastQuestion/LastQuestion.js
+++ client/src/components/LastQuestion/LastQuestion.js
@@ -2,28 +2,8 @@
 import React from 'react'
 import Question from '../Question/Question'
 
-const LastQuestion = ({input, data}) => {
-import React, {useState, useEffect} from 'react'
-import SearchBar from '../SearchBar/SearchBar'
+const LastQuestion = ({data}) => {
 
-const LastQuestion = ({input}) => {
-
-    const qsData = async ()=> {
-        const response = await fetch('http://localhost:8000/api/getAllQ')
-        const answer = await response.json()
-        setData(answer)
-    }
-
-    const dataSort = ()=> {
-      data.forEach(item=> item.date.sort((a,b)=>{return a-b}))
-    }
-console.log(dataSort());
-
-    useEffect(()=>{
-        qsData();
-        }, [])
-
-        const lastQuestions = data.map(prop=> <div key={prop.id}>{prop.q}</div>).sort((a,b)=> {return a-b})
   return (
     <div>
         {data.map((q)=> 
