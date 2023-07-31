4d2c62d21272d16bf8bf64fa3156bf7a7b99ad8a
Whygal <87670165+Whygal@users.noreply.github.com>
2023-07-31 14:28:49 +0300

    Delete Const.js

--- client/src/Constants/Const.js
+++
@@ -1,2 +0,0 @@
-export const BASE_URL = process.env.NODE_ENV === "development" ? 'http://localhost:8000' : 'something from render'
-export const ApiKey = process.env.NODE_ENV === "development" ? 'AIzaSyAVnKGC0Z9F9UzxiTQ6b4JJhCj6ScJbJT0' : 'something from render'
