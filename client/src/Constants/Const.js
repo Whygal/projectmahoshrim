65fe44e7140146c58e682668379186b6c1e7917d
Whygal <yanivgalili18@gmail.com>
2023-10-03 20:01:29 +0300

    midware fetch

--- client/src/Constants/Const.js
+++ client/src/Constants/Const.js
@@ -1,2 +1,2 @@
 export const BASE_URL = process.env.NODE_ENV === "development" ? "http://localhost:8000" : ""
-export const ApiKey = process.env.NODE_ENV === "development" ? "AIzaSyAVnKGC0Z9F9UzxiTQ6b4JJhCj6ScJbJT0" : ""
+
